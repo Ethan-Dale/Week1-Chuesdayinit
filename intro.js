fe7bcdd777b7eb5f8d4f00cb9d0c6a618d0dcc8a
Ethan Dale <ethan.dale.147@gmail.com>
2023-02-28 15:41:35 -0700

    new demo file

---
+++ intro.js
@@ -0,0 +1,38 @@
+// console.log("Hello, world")
+// console.log("I am really enjoying this class and the people in it.")
+
+const firstName = "Ethan"
+// console.log(firstName)
+const lastName = "Dale"
+// console.log(lastName)
+let age = 24
+const isCool = true
+// console.log(firstName, age, isCool)
+
+
+// console.log("Hello, my name is " + firstName + " I am " + age + " years old.")
+// console.log(`Hello, my name is ${firstName} I am ${age} years old.`)
+
+let x = 4
+let y = 10 
+let z = x + 8
+let sum = x + y + z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// console.log(sum)
+
+// if (condition to check){
+// code to run
+// }
+if (x === y){
+    console.log('X and Y are the same value')
+}   else {
+    console.log('X and Y are different values')
+}
+
+if (x < 10){
+    console.log('X is smaller than Y')
+}
